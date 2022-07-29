1840da8914b05480b0f706d393d3ff22ffac113e
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2022-07-29 12:09:57 -0400

    working on cours page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+
+  get '/cours', to: 'pages#cours', as: :cours
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
